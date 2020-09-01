3f5997de06cbee672b03e7498975f5ede8ffa58c
Kate Kharitonova <katekharitonova@corp.badoo.com>
2020-09-01 17:05:23 +0300

    print local and remote time

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	localTime := time.Now()
+	fmt.Printf("current time: %v\n", localTime.Round(0))
+
+	remoteTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatalf("Error getting remote time: %v", err)
+	}
+	fmt.Printf("exact time: %v\n", remoteTime.Round(0))
 }
